e035020677af8d0ead0eb35aad17023a0adcb6ab
Frank Pham <fhpham@uci.edu>
2017-11-20 21:45:53 -0800

    fixed typos

--- loss/lossfunctions.py
+++ loss/lossfunctions.py
@@ -8,31 +8,34 @@ import numpy as np
     n_itr - number of iteration
     len - length of data
 '''
-def loss_fashion_landmarks(l, v, l_p, v_p, n_itr, len, alpha = 0.5):
-        t1 = 2000
-        t2 = 400
 
-        loss = 0
-        loss_landmarks = 0
-        loss_visibilities = 0
-        
-        if n_itr < t1 :
-            alpha = alpha 
-        elif t1 <= n_int < t2:
-            alpha = alpha*((n_itr-t1)/(t2-t1))
-        else:
-            alpha = 0
 
-        #Calculate Euclidient Loss for Visibility, Landmark Positions
-        for i in range(1, len):
-            loss_landmarks += np.sqrt(np.linalg.norm(v_l-l))
-            loss_visibilities += np.sqrt(np.linalg.norm(v_p-v))
-        
-        loss_landmarks = loss_landmarks/(2*len)
-        loss_visibilities = loss_visibilities/(2*len)
+def loss_fashion_landmarks(l, v, l_p, v_p, n_itr, len, alpha=0.5):
+    t1 = 2000
+    t2 = 400
 
-    loss = loss_landmarks + (alpha*loass_visibilities)
+    loss = 0
+    loss_landmarks = 0
+    loss_visibilities = 0
+
+    if n_itr < t1:
+        alpha = alpha
+    elif t1 <= n_itr < t2:
+        alpha = alpha * ((n_itr - t1) / (t2 - t1))
+    else:
+        alpha = 0
+
+    #Calculate Euclidient Loss for Visibility, Landmark Positions
+    for i in range(1, len):
+        loss_landmarks += np.sqrt(np.linalg.norm(l_p - l))
+        loss_visibilities += np.sqrt(np.linalg.norm(v_p - v))
+
+    loss_landmarks = loss_landmarks / (2 * len)
+    loss_visibilities = loss_visibilities / (2 * len)
+
+    loss = loss_landmarks + (alpha * loss_visibilities)
     return loss
 
-def loss_fashion_net:
+
+def loss_fashion_net():
     pass
