da8e16dc549360d5481507494949c760854652b3
Frank Pham <fhpham@uci.edu>
2017-11-30 16:17:57 -0800

    added comments

--- model/FashionNet.py
+++ model/FashionNet.py
@@ -5,50 +5,79 @@ import torchvision
 
 class FashionNet(nn.Module):
     """
-    FashionNet model"""
+    Modified FashionNet: use model to find the landmarks of clothing
+    """
     def __init__(self, vgg_features, pose_features, num_outputs):
+        """
+        Constructor for FashionNet class
+
+        :param vgg_features: Feature layers of VGG-16
+        :param pose_features: Layers for pose
+        :param num_outputs: Number of landmarks
+        """
         super().__init__()
+        # Multiply by 2 because each landmark needs xy coordinate
         outputs = num_outputs * 2
+        # each landmark has one of three visibilities: visible, partially visible, not visible
+        classes = num_outputs * 3
+
+        # Create the model
+        # Save pretrained model
         self.vgg_features = vgg_features
+        # Save the layers for pose
         self.pose_features = pose_features
+        # Create layers for fully connected pose layers
         self.fc_pose = nn.Sequential(
+            # Linear layer. Inputs = 25,088, Outputs = 8
             nn.Linear(512 * 7 * 7, 1),
-            nn.ReLU(True),
+            # ReLU activation layer
+            nn.ReLU(inplace=True),
+            # Linear Layer. Inputs = 1, Outputs = 1
             nn.Linear(1, 1),
-            nn.ReLU(True)
+            # ReLU activation layer
+            nn.ReLU(inplace=True)
         )
 
+        # Output layer for landmark locations
         self.location = nn.Sequential(
+            # Linear layer. Inputs: 1, Outputs: num_outputs * 2
             nn.Linear(1, outputs)
         )
 
+        # Output layer for landmark visibility
         self.visibility = nn.Sequential(
-            nn.Linear(outputs, outputs),
+            # Linear Layer. Inputs (Landmark locations): num_outputs * 2, Outputs: num_outputs * 3
+            nn.Linear(outputs, classes),
+            # Softmax activation
             nn.Softmax(),
-            nn.Linear(outputs, outputs),
+            # Linear Layer. Inputs = num_outputs * 3, Outputs = num_outputs * 3
+            nn.Linear(classes, classes),
+            # Softmax activation
             nn.Softmax(),
-            nn.Linear(outputs, outputs),
+            # Linear Layer. Inputs = num_outputs * 3, Outputs = num_outputs * 3
+            nn.Linear(classes, classes),
+            # Softmax activation
             nn.Softmax(),
-            nn.Linear(outputs, outputs),
+            # Linear Layer. Inputs = num_outputs * 3, Outputs = num_outputs * 3
+            nn.Linear(classes, classes),
+            # Softmax activation
             nn.Softmax()
         )
 
-
+    # Forward pass through network
     def forward(self, x):
+        # Send inputs through VGG
         x = self.vgg_features(x)
+        # Send result from VGG to pose_features layers
         x = self.pose_features(x)
+        # flatten
         x = x.view(x.size(0), - 1)
+        # Send flattened to fc_pose layers
         x = self.fc_pose(x)
+        # get the landmarks
         landmarks = self.location(x)
+        # get visibility of landmarks
         vis = self.visibility(landmarks)
-        
-        # vis = []
-
-        # c, v = landmarks.size()
-
-        # for i in range(0, c):
-        #     for j in range(0, v, 2):
-        #         vis.append(self.visibility(landmarks[i][j:j+2]))
 
         return landmarks, vis
 
@@ -85,20 +114,25 @@ def make_landmark_pose_layers(batch_norm=False):
 
 
 def landmark_network(batch_norm=False, num_outputs=8):
+    """Create a new instace of FashionNet
+    
+    :param batch_norm: A boolean deciding if batch normalization layers
+    :param num_outputs: A integer for the number of landmarks"""
+
+    # Get pretrained VGG-16
     vgg = features(batch_norm=batch_norm)
 
+    # set VGG-16 weights to not be updated
     for param in vgg.parameters():
         param.requires_grad = False
 
+    # Make the pose layers
     pose = make_landmark_pose_layers(batch_norm=batch_norm)
+    # Create a new instace of FashionNet
     model = FashionNet(vgg, pose, num_outputs)
 
+    # Check if cuda is available, if so, use it.
     if torch.cuda.is_available():
         model = model.cuda()
 
     return model
-
-
-if __name__ == '__main__':
-    m = landmark_network(num_outputs=6)
-    print(m)
