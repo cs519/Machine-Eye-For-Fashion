31a5d2589dc87d8bf56075580af95eb4bdd983aa
Frank Pham <fhpham@uci.edu>
2017-12-03 22:20:55 -0800

    formatting

--- preprocessing/dataloader.py
+++ preprocessing/dataloader.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
 
+
 class FashionLandmarkDataset(Dataset):
     """Fashion Landmark dataset."""
 
@@ -26,10 +27,13 @@ class FashionLandmarkDataset(Dataset):
 
     def __getitem__(self, idx):
         # print('TYRING TO OPEN FILE: {}'.format(os.path.join(self.root_dir, '{}.jpg'.format(str(self.landmarks_frame.ix[idx, 0])))))
-        image_name = os.path.join(self.root_dir, '{}.jpg'.format(str(self.landmarks_frame.ix[idx, 0])))
+        image_name = os.path.join(self.root_dir, '{}.jpg'.format(
+            str(self.landmarks_frame.ix[idx, 0])))
         image = Image.open(image_name).convert('RGB')
-        landmarks = self.landmarks_frame.ix[idx, 3:14].as_matrix().astype('float32')
-        visibility = self.landmarks_frame.ix[idx, 15:].as_matrix().astype('int64')
+        landmarks = self.landmarks_frame.ix[idx, 3:14].as_matrix().astype(
+            'float32')
+        visibility = self.landmarks_frame.ix[idx, 15:].as_matrix().astype(
+            'int64')
 
         if self.transform is not None:
             image = self.transform(image)
